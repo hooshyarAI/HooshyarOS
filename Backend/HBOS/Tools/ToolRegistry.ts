f1443dfa6aadfc982f0b2a668bf5c90077b2b552
hooshyarAI <mohamadali.kamali@gmail.com>
2026-08-07 02:08:13 +0330

    feat(hbos): add tool registry core

---
+++ Backend/HBOS/Tools/ToolRegistry.ts
@@ -0,0 +1,23 @@
+﻿export class ToolRegistry {
+
+private tools:any[] = [];
+
+register(tool:any){
+
+    this.tools.push(tool);
+
+}
+
+getTools(){
+
+    return this.tools;
+
+}
+
+find(name:string){
+
+    return this.tools.find(t => t.name === name);
+
+}
+
+}
