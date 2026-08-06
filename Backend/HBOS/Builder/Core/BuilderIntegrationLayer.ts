91e5247ece30f98502250ea0cb77cf8b45b73636
hooshyarAI <mohamadali.kamali@gmail.com>
2026-08-07 01:55:56 +0330

    feat(builder): add final integration layer

---
+++ Backend/HBOS/Builder/Core/BuilderIntegrationLayer.ts
@@ -0,0 +1,20 @@
+﻿export class BuilderIntegrationLayer {
+
+private modules:any[] = [];
+
+register(module:any){
+
+    this.modules.push(module);
+
+}
+
+status(){
+
+    return {
+        modules:this.modules.length,
+        state:"integrated"
+    };
+
+}
+
+}
