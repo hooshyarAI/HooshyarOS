a746deec65e5cfa45ec97172f0b9c0c56d0534e5
hooshyarAI <mohamadali.kamali@gmail.com>
2026-08-07 01:45:38 +0330

    feat(builder): add knowledge graph core

---
+++ Backend/HBOS/Builder/Knowledge/BuilderKnowledgeGraph.ts
@@ -0,0 +1,27 @@
+﻿export class BuilderKnowledgeGraph {
+
+private nodes:any[] = [];
+private relations:any[] = [];
+
+addNode(node:any){
+
+    this.nodes.push(node);
+
+}
+
+addRelation(relation:any){
+
+    this.relations.push(relation);
+
+}
+
+getKnowledge(){
+
+    return {
+        nodes:this.nodes,
+        relations:this.relations
+    };
+
+}
+
+}
