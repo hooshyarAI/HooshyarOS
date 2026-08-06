2f4b9569e3c3427efa61698c75d60f66c1f56081
hooshyarAI <mohamadali.kamali@gmail.com>
2026-08-07 01:34:15 +0330

    feat(builder): add learning engine core

---
+++ Backend/HBOS/Builder/Learning/BuilderLearningEngine.ts
@@ -0,0 +1,20 @@
+﻿export class BuilderLearningEngine {
+
+private lessons:any[] = [];
+
+learn(event:any){
+
+    this.lessons.push({
+        event,
+        timestamp: new Date()
+    });
+
+}
+
+getLessons(){
+
+    return this.lessons;
+
+}
+
+}
