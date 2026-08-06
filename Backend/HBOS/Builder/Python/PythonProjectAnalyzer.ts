ad68c3b62831ac6dbf697079bd6a613f5201bfd8
hooshyarAI <mohamadali.kamali@gmail.com>
2026-08-07 01:52:46 +0330

    feat(builder): add python project analyzer

---
+++ Backend/HBOS/Builder/Python/PythonProjectAnalyzer.ts
@@ -0,0 +1,14 @@
+﻿export class PythonProjectAnalyzer {
+
+scan(project:any){
+
+    return {
+        project,
+        engines: project.engines || [],
+        modules: project.modules || [],
+        status:"scanned"
+    };
+
+}
+
+}
