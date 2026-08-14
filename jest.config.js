5e43dd635d0362193df70285470d479395061a0d
hooshyarAI <mohamadali.kamali@gmail.com>
2026-08-14 10:43:31 +0330

    fix(test): exclude productization payloads from canonical Jest suite

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,6 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     cache: false,
+    roots: ["<rootDir>/Backend"],
+    testPathIgnorePatterns: ["/node_modules/", "/dist/productization/"],
 };
